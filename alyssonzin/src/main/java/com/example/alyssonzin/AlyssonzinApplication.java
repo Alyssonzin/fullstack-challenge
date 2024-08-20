e98fe8be41e0c2ba0598021c704a3a13a038a893
Alyssonzin <106278944+Alyssonzin@users.noreply.github.com>
2024-08-20 15:31:36 -0300

    Add files via upload

---
+++ alyssonzin/src/main/java/com/example/alyssonzin/AlyssonzinApplication.java
@@ -0,0 +1,13 @@
+package com.example.alyssonzin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlyssonzinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlyssonzinApplication.class, args);
+	}
+
+}
---
+++ alyssonzin/src/test/java/com/example/alyssonzin/AlyssonzinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alyssonzin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlyssonzinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

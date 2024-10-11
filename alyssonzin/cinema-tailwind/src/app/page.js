5c61174432b90188ff3c4e570c4cc6179dea3eb0
Alysson <alyssonempire84@gmail.com>
2024-10-11 17:03:19 -0300

    Remove unnecessary .idea files and update movie listing on home page

--- alyssonzin/cinema-tailwind/src/app/page.js
+++ alyssonzin/cinema-tailwind/src/app/page.js
@@ -15,14 +15,25 @@ export default function Home() {
         language: 'pt-br'
       }
     }).then((response) => {
-      console.log(response);
+      setMovies(response.data.results);
     })
 
-  });
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <h1 className="text-4xl font-bold">Filmes em cartaz</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {movies.map((movie) => (
+          <div key={movie.id} className="bg-white shadow-lg rounded-lg overflow-hidden">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full" />
+            <div className="p-4">
+              <h2 className="font-bold text-xl">{movie.title}</h2>
+              <p className="text-gray-500">{movie.overview}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }

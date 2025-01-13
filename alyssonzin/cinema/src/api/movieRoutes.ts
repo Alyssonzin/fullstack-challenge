af7fc721adaeb90edc5b5488cc9754f75df3e2b8
Alysson <alyssonempire84@gmail.com>
2025-01-13 17:26:48 -0300

    Adicionando parâmetro "region" nas requisições API

--- alyssonzin/cinema/src/api/movieRoutes.ts
+++ alyssonzin/cinema/src/api/movieRoutes.ts
@@ -6,6 +6,7 @@ export const getNowPlaying = async () => {
     return apiTMDB.get<TMDBresponse>('/now_playing', {
         params: {
             language: 'pt-BR',
+            region: 'BR',
         }
     }).then(res => {
         return res.data.results;

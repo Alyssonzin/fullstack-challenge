842b2df5c7ce98eb163d320f63ef6342f3c390a9
Alyssonzin <alyssonempire84@gmail.com>
2025-01-13 22:00:18 -0300

    Expansão suave do card

--- alyssonzin/cinema/src/components/MovieCard.tsx
+++ alyssonzin/cinema/src/components/MovieCard.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 import MovieType from "../types/MovieType";
 import { ChevronUp } from 'lucide-react';
 import Link from "next/link";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface MovieCardProps {
     movieProps: MovieType;
 }
 
-export default function MovieCard({ movieProps: movie }: MovieCardProps) { 
+export default function MovieCard({ movieProps: movie }: MovieCardProps) {
     const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const refContent = useRef<HTMLDivElement>(null);
 
-    function rotateChevron() {
+    function expandCard() {
         setIsExpanded(!isExpanded);
     }
-    
+
     return (
         <article className="select-none bg-slate-300 shadow-md border border-gray-400 hover:bg-slate-400 transition rounded p-2">
             <div className="mb-2">
@@ -31,12 +32,21 @@ export default function MovieCard({ movieProps: movie }: MovieCardProps) {
             <div>
                 <h3 className='leading-5 font-semibold font-sans mb-3'>{movie.title}</h3>
                 <span
-                    className='font-semibold text-sm w-fit hover:cursor-pointer hover:underline'
-                    onClick={rotateChevron}
+                    className='font-semibold text-sm w-fit hover:underline cursor-pointer'
+                    onClick={expandCard}
                 >
                     Mais detalhes
                     <ChevronUp className={`inline size-4 align-top mt-0.5 transition ${isExpanded ? 'rotate-180' : ''}`} />
                 </span>
+            </div>
+            <div
+                ref={refContent}
+                className='overflow-hidden transition-all duration-300'
+                style={{
+                    height: isExpanded ? `${refContent.current?.scrollHeight}px` : '0px',
+                }}
+            >
+                <p className='mt-3'>{movie.overview}</p>
             </div>
         </article>
     );

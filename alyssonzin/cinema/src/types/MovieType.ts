043f6dfa93350e53aa58d0c10e3f246cf7ac2c41
Alysson <alyssonempire84@gmail.com>
2025-01-15 17:42:44 -0300

    ajustando tipagem de MovieType e ProductionCompany

--- alyssonzin/cinema/src/types/MovieType.ts
+++ alyssonzin/cinema/src/types/MovieType.ts
@@ -1,8 +1,14 @@
+import ProductionCompany from "./ProductionCompany";
+
 export default interface MovieType {
-    id: string, 
+    id: string,
     title: string,
+    original_title: string,
     overview: string,
     poster_path: string,
     release_date: string,
     vote_average: number,
+    production_companies?: ProductionCompany[],
+    genres?: { id: number, name: string }[],
+    runtime?: number,
 };

a1f712d6b844be6a9705669197dd7092f8a451b1
Alysson <alyssonempire84@gmail.com>
2025-01-16 10:50:54 -0300

    Resolvido: consegui buscar os detalhes do filme na página de detalhes do mesmo sem usar useEffect

--- alyssonzin/cinema/src/app/[movieId]/page.tsx
+++ alyssonzin/cinema/src/app/[movieId]/page.tsx
@@ -1,14 +1,21 @@
+import { getMovieDetails } from "../../api/movieRoutes";
 
-export default async function MoviePage({
-    params,
-}: {
-    params: Promise<{ movieId: string }>
-}) {
+interface Props {
+    params: Promise<{ movieId: string }>,
+}
+
+function fetchMovie(movieId: string) {
+    return getMovieDetails(movieId);
+}
+
+export default async function MoviePage({ params }: Props) {
     const { movieId } = await params;
+    const movie = await fetchMovie(movieId);
 
     return (
         <main>
-            {movieId}
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
         </main>
     );
 }
--- alyssonzin/cinema/src/app/[movieId]/route.ts
+++
@@ -1,8 +0,0 @@
-import { getMovieDetails } from "../../api/movieRoutes";
-
-export async function GET({ params }: { params: { movieId: string } }) {
-    const { movieId } = params;
-    const movieDetails = await getMovieDetails(movieId);
-
-    return movieDetails;
-}

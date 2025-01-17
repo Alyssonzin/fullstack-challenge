928ac863f8cb9fb8211f4ba8f771303438d6abe8
Alysson <alyssonempire84@gmail.com>
2025-01-17 17:31:32 -0300

    atualiza cores e tipagens da classificação indicativa de filmes

--- alyssonzin/cinema/src/components/MoviePage/IndicativeRating.tsx
+++ alyssonzin/cinema/src/components/MoviePage/IndicativeRating.tsx
@@ -9,7 +9,8 @@ export default function IndicativeRating({ certification }: IndicativeRatingProp
     const element = IndicativeRatingsHashMap.get(certification);
 
     return (
-        <span className={`flex justify-center items-center min-h-6 min-w-6 rounded font-bold ${element?.color || 'bg-green-500'}`}>
+        <span className='flex justify-center items-center min-h-6 min-w-6 rounded font-bold'
+        style={{ backgroundColor: element?.color }}>
             {certification}
         </span>
     )
--- alyssonzin/cinema/src/types/IndicativeRatings.ts
+++ alyssonzin/cinema/src/types/IndicativeRatings.ts
@@ -1,7 +1,8 @@
 export const IndicativeRatingsHashMap = new Map([
-    ['10', { minimumAge: 10, color: 'bg-blue-500' }],
-    ['12', { minimumAge: 12, color: 'bg-yellow-500' }],
-    ['14', { minimumAge: 14, color: 'bg-orange-500' }],
-    ['16', { minimumAge: 16, color: 'bg-red-500' }],
-    ['18', { minimumAge: 18, color: 'bg-black' }],
+    ['L', { minimumAge: 0, color: 'rgb(16 185 129 / var(--tw-bg-opacity, 1))' }],
+    ['10', { minimumAge: 10, color: 'rgb(59 130 246 / var(--tw-bg-opacity, 1))' }],
+    ['12', { minimumAge: 12, color: 'rgb(239 68 68 / var(--tw-bg-opacity, 1))' }],
+    ['14', { minimumAge: 14, color: 'rgb(249 115 22 / var(--tw-bg-opacity, 1))' }],
+    ['16', { minimumAge: 16, color: 'rgb(239 68 68 / var(--tw-bg-opacity, 1))' }],
+    ['18', { minimumAge: 18, color: 'black' }],
 ]);

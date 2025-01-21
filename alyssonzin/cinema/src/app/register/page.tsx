a64c1cc67d3ec5bd472bf29611e1cad97f610db9
Alyssonzin <alyssonempire84@gmail.com>
2025-01-21 20:45:37 -0300

    Exemplo de formulario básico

--- alyssonzin/cinema/src/app/register/page.tsx
+++ alyssonzin/cinema/src/app/register/page.tsx
@@ -1,13 +1,35 @@
-import { useForm } from "react-hook-form";
+'use client';
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+    name: string;
+    cpf: string;
+}
+
+const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+}
+
+const onErrorSubmit: SubmitErrorHandler<FormData> = (errors) => {
+    console.log(errors);
+}
 
 export default function Register() {
-    const { register, handleSubmit, setValue } = useForm();
-    
+    const { register, handleSubmit, getValues } = useForm<FormData>();
     return(
-        <main className="min-h-screen text-white">
-            <div>
-                <form action="">
-                    
+        <main className="min-h-screen">
+            <div className="flex flex-col items-center justify-center">
+                <form onSubmit={handleSubmit(onSubmit, onErrorSubmit)}>
+                    <div className="flex space-y-3 flex-col">
+                        <label>
+                            <input className="p-1 rounded" {...register('name')} type="text" placeholder="Nome completo" />
+                        </label>
+                        <label>
+                            <input className="p-1 rounded" {...register('cpf', {required: true})} type="text" placeholder="CPF" />
+                        </label>
+
+                        <button className="bg-blue-500 text-white rounded p-1" type="submit">Cadastrar</button>
+                    </div>
                 </form>
             </div>
         </main>

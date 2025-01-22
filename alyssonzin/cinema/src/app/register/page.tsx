d6718dc54549a767237a7714c31e139dfe45334c
Alysson <alyssonempire84@gmail.com>
2025-01-22 10:54:19 -0300

    Tratando campos no form

--- alyssonzin/cinema/src/app/register/page.tsx
+++ alyssonzin/cinema/src/app/register/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface FormData {
     name: string;
@@ -10,23 +10,34 @@ const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
 }
 
-const onErrorSubmit: SubmitErrorHandler<FormData> = (errors) => {
-    console.log(errors);
-}
-
 export default function Register() {
-    const { register, handleSubmit, getValues } = useForm<FormData>();
-    return(
+    const { register, handleSubmit, formState } = useForm<FormData>();
+    return (
         <main className="min-h-screen">
             <div className="flex flex-col items-center justify-center">
-                <form onSubmit={handleSubmit(onSubmit, onErrorSubmit)}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="flex space-y-3 flex-col">
-                        <label>
-                            <input className="p-1 rounded" {...register('name')} type="text" placeholder="Nome completo" />
-                        </label>
-                        <label>
-                            <input className="p-1 rounded" {...register('cpf', {required: true})} type="text" placeholder="CPF" />
-                        </label>
+                        <div>
+                            <input
+                                className="p-1 rounded"
+                                {...register('name', { required: true })}
+                                type="text"
+                                autoComplete="off"
+                                placeholder="Nome completo"
+                            />
+                            {formState.errors.name && <p className="text-red-500">Nome é obrigatório</p>}
+                        </div>
+
+                        <div>
+                            <input
+                                className="p-1 rounded"
+                                {...register('cpf', { required: true })}
+                                type="text"
+                                autoComplete="off"
+                                placeholder="CPF"
+                            />
+                            {formState.errors.cpf && <p className="text-red-500">CPF é obrigatório</p>}
+                        </div>
 
                         <button className="bg-blue-500 text-white rounded p-1" type="submit">Cadastrar</button>
                     </div>

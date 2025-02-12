9088d6ed63369895353d298dad879dffbb7766d3
Alysson <alyssonempire84@gmail.com>
2025-02-12 11:12:36 -0300

    Atualiza as cores das classificações indicativas para o formato oklch

--- alyssonzin/cinema/src/types/IndicativeRatings.ts
+++ alyssonzin/cinema/src/types/IndicativeRatings.ts
@@ -1,8 +1,8 @@
 export const IndicativeRatingsHashMap = new Map([
-    ['', { minimumAge: 0, simbol: 'L', text: 'Livre para todos os públicos', color: 'rgb(16 185 129 / var(--tw-bg-opacity, 1))' }],
-    ['10', { minimumAge: 10, simbol: '10', text: 'Não recomendado para menores de 10 anos', color: 'rgb(59 130 246 / var(--tw-bg-opacity, 1))' }],
-    ['12', { minimumAge: 12, simbol: '12', text: 'Não recomendado para menores de 12 anos', color: 'rgb(239 68 68 / var(--tw-bg-opacity, 1))' }],
-    ['14', { minimumAge: 14, simbol: '14', text: 'Não recomendado para menores de 14 anos', color: 'rgb(249 115 22 / var(--tw-bg-opacity, 1))' }],
-    ['16', { minimumAge: 16, simbol: '16', text: 'Não recomendado para menores de 16 anos', color: 'rgb(239 68 68 / var(--tw-bg-opacity, 1))' }],
+    ['', { minimumAge: 0, simbol: 'L', text: 'Livre para todos os públicos', color: 'oklch(0.723 0.219 149.579)' }],
+    ['10', { minimumAge: 10, simbol: '10', text: 'Não recomendado para menores de 10 anos', color: 'oklch(0.623 0.214 259.815)' }],
+    ['12', { minimumAge: 12, simbol: '12', text: 'Não recomendado para menores de 12 anos', color: 'oklch(0.795 0.184 86.047)' }],
+    ['14', { minimumAge: 14, simbol: '14', text: 'Não recomendado para menores de 14 anos', color: 'oklch(0.705 0.213 47.604)' }],
+    ['16', { minimumAge: 16, simbol: '16', text: 'Não recomendado para menores de 16 anos', color: 'oklch(0.637 0.237 25.331)' }],
     ['18', { minimumAge: 18, simbol: '18', text: 'Não recomendado para menores de 18 anos', color: 'black' }],
 ]);

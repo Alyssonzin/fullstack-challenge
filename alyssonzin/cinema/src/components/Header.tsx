9df9801a33f2df1db22d1ca88c17c925a8f577e9
Alyssonzin <alyssonempire84@gmail.com>
2025-02-13 19:28:17 -0300

    Unifica botao de login e registro

--- alyssonzin/cinema/src/components/Header.tsx
+++ alyssonzin/cinema/src/components/Header.tsx
@@ -2,27 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="text-white bg-cyan-950 flex justify-between p-4">
-        <Link href="/" className="contents">
-          <Image
-            src="/cinema-logo.png"
-            alt="Cinema"
-            width={70}
-            height={100}
-          />
-        </Link>
+  return (
+    <header className="text-white bg-cyan-950 flex justify-between p-4">
+      <Link href="/" className="contents">
+        <Image
+          src="/cinema-logo.png"
+          alt="Cinema"
+          width={70}
+          height={100}
+        />
+      </Link>
 
-        <nav className="flex items-center m-4">
-          <ul>
-            <li>
-              <Link href="/login">Login</Link>
-            </li>
-            <li>
-              <Link href="/register">Cadastrar-se</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
+      <nav className="m-1">
+        <ul className="flex items-center space-x-4">
+          <li>
+            <Link href="/register">
+              <div className="hover:bg-sky-600 transition rounded p-2">
+                <p>Entre ou <br /> Cadastre-se</p>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }

3ca40d275db7ce8fdb066abf441a97abc479d089
Alysson <alyssonempire84@gmail.com>
2025-02-14 11:53:01 -0300

    Remove propriedades desnecessárias do componente Input e adiciona autoComplete

--- alyssonzin/cinema/src/components/formComponents/Input.tsx
+++ alyssonzin/cinema/src/components/formComponents/Input.tsx
@@ -1,20 +1,17 @@
 import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    type: string,
-    placeholder?: string,
     label?: string,
     isError?: boolean,
 }
 
-export default function Input({ type, label, placeholder, isError, ...rest }: InputProps) {
+export default function Input({ label, isError, ...rest }: InputProps) {
     return (
         <div>
             {label && <label>{label}</label>}
             <input
-                className={`w-full text-black mt-1 mb-1 p-2 rounded outline-none ${isError? 'outline-red-500': 'outline-cyan-600'}`}
-                placeholder={placeholder}
-                type={type}
+                className={`w-full text-black mt-1 mb-1 p-2 rounded outline-none ${isError ? 'outline-red-500' : 'outline-cyan-600'}`}
+                autoComplete="off"
                 {...rest}
             />
         </div>
